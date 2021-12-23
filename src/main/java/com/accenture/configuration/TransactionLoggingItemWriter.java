6a1d1340c6792a5e34baab28fc06025d4527dbfc
Gabriel Manay <eduardo.g.manay@CPX-YTI6JGU2YGG.dir.svc.accenture.com>
2021-12-23 15:08:42 -0300

    se actualiza ItemWriter añadiendo carga de lineas en TransactionDTO

--- src/main/java/com/accenture/configuration/TransactionLoggingItemWriter.java
+++ src/main/java/com/accenture/configuration/TransactionLoggingItemWriter.java
@@ -1,26 +1,34 @@
 package com.accenture.configuration;
 
-import com.accenture.entity.Transaction;
+import java.util.List;
+import java.util.Properties;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
 
-
-import java.util.List;
-import java.util.Properties;
+import com.accenture.entity.Transaction;
+import com.accenture.entity.TransactionDTO;
 
 public class TransactionLoggingItemWriter implements ItemWriter<Transaction> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TransactionLoggingItemWriter.class);
+    
+    @Autowired
+    TransactionDTO transactionDTO;
 
     @Override
     public void write(List<? extends Transaction> items) throws Exception {
-        sendMessage(items.get(0).toString());
+    	
+    	transactionDTO.addTransaction(items.get(0));
+    	
+    	//sendMessage(items.get(0).toString());
     }
-
+/*
     private void sendMessage(String message) {
         LOGGER.info("************* Se envia mensaje: {}", message);
 
@@ -35,4 +43,5 @@ public class TransactionLoggingItemWriter implements ItemWriter<Transaction> {
             producer.flush();
         }
     }
+    */
 }
