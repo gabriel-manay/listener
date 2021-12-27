40ab9450dfab12c009c25b76256af12ea4143f67
Gabriel Manay <eduardo.g.manay@host.docker.internal>
2021-12-27 09:37:53 -0300

    se remueven comentarios

--- src/main/java/com/accenture/configuration/TransactionLoggingItemWriter.java
+++ src/main/java/com/accenture/configuration/TransactionLoggingItemWriter.java
@@ -1,13 +1,7 @@
 package com.accenture.configuration;
 
 import java.util.List;
-import java.util.Properties;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,32 +10,14 @@ import com.accenture.entity.TransactionDTO;
 
 public class TransactionLoggingItemWriter implements ItemWriter<Transaction> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TransactionLoggingItemWriter.class);
-    
-    @Autowired
-    TransactionDTO transactionDTO;
+	@Autowired
+	TransactionDTO transactionDTO;
 
-    @Override
-    public void write(List<? extends Transaction> items) throws Exception {
-    	
-    	transactionDTO.addTransaction(items.get(0));
-    	
-    	//sendMessage(items.get(0).toString());
-    }
-/*
-    private void sendMessage(String message) {
-        LOGGER.info("************* Se envia mensaje: {}", message);
+	@Override
+	public void write(List<? extends Transaction> items) throws Exception {
 
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "kafka-broker:9092");
-        props.put("acks", "0");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		transactionDTO.addTransaction(items.get(0));
 
-        try (Producer<String, String> producer = new KafkaProducer<>(props);) {
-            producer.send(new ProducerRecord<>("channel01", "key", message));
-            producer.flush();
-        }
-    }
-    */
+	}
+
 }
